a9cd616b24c00af573839e5dfa3ed82bea6a0045
chuckfairy <charlieabeling@gmail.com>
2018-09-19 17:21:33 -0500

    updates to necktoools

--- src/qt/Instrument/Neck.cpp
+++ src/qt/Instrument/Neck.cpp
@@ -5,7 +5,20 @@
 
 namespace Orza { namespace Steel { namespace Instrument {
 
+/**
+ * constructor
+ */
+
 Neck::Neck() {
 };
+
+
+/**
+ * Positions getter
+ */
+
+vector<uint8_t> Neck::getPositions() {
+
+}
 
 } } }
--- src/qt/Instrument/Neck.h
+++ src/qt/Instrument/Neck.h
@@ -3,8 +3,15 @@
  */
 #pragma once
 
+#include <cmath>
+#include <cstdint>
+#include <vector>
+
 #include <QtGui>
 #include <QWidget>
+
+
+using std::vector;
 
 
 namespace Orza { namespace Steel { namespace Instrument {
@@ -17,6 +24,25 @@ class Neck : public QWidget {
 
 		Neck();
 		~Neck() {};
+
+		/**
+		 * setter for positions creation
+		 */
+
+		void setPositions( uint8_t );
+
+		/**
+		 * Get positions from bridge setup
+		 */
+
+		vector<uint8_t> getPositions();
+
+
+	private:
+
+		vector<uint8_t> _positions;
+
+		void createBridgeArea( uint8_t );
 
 };
 
